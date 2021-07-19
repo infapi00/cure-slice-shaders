cd697c633b520c9e3292847e44bc79f389fcef60
Alejandro Piñeiro <apinheiro@igalia.com>
2021-07-19 11:13:42 +0100

    GS - remove use and definition of v_line_width/height => still hangs

--- GS_32ac035bab2cbeb12da175066c98b8b14e5617d4.glsl
+++ GS_32ac035bab2cbeb12da175066c98b8b14e5617d4.glsl
@@ -10,8 +10,6 @@ layout(lines) in;
 layout(triangle_strip, max_vertices = 15) out;
 
 in lowp vec4 v_color[];
-in mediump float v_line_width[];
-in mediump float v_line_height[];
 
 out lowp vec4 f_color;
 out vec3 f_normal;
@@ -41,8 +39,8 @@ viewProjectionMatrix = u_projectionMatrix * u_viewMatrix;
 vec3 vertex_delta = gl_in[1].gl_Position.xyz - gl_in[0].gl_Position.xyz;
 vec3 normal_h = normalize(vec3(vertex_delta.z, vertex_delta.y, -vertex_delta.x));
 vec3 normal_v = vec3(0.0, 1.0, 0.0);
-vec4 offset_h = vec4(normal_h * v_line_width[1], 0.0);
-vec4 offset_v = vec4(normal_v * v_line_height[1], 0.0);
+vec4 offset_h = vec4(normal_h, 0.0);
+vec4 offset_v = vec4(normal_v, 0.0);
 
 outputEdge(normal_v, offset_v);
 outputEdge(normal_h, offset_h);
